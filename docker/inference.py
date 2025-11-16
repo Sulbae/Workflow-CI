50b145d0703a6c12ba1f40aab1aa3f92b9c061c5
Sulbae <assulis500@gmail.com>
2025-11-16 21:49:33 +0700

    update inference.py

--- docker/inference.py
+++ docker/inference.py
@@ -1,16 +1,13 @@
 import mlflow.pyfunc
 import pandas as pd
-import joblib
-import numpy as np
-import pandas as pd
-import requests
-import json
+import os
 
-MODEL_PATH = "model/best_model"
-TEST_FILE = "sample_test.csv"
+# Konfigurasi
+TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI", None)
+MODEL_URI = os.getenv("MODEL_URI", "model/best_model")
+TEST_FILE = os.getenv("TEST_FILE", "sample_test.csv")
 
-model = mlflow.pyfunc.load_model(MODEL_PATH)
-
+# Features
 COLUMNS = [
     "ph",
     "Hardness",
@@ -23,13 +20,24 @@ COLUMNS = [
     "Turbidity"
 ]
 
-def prediction(data_path: str):
+def load_model():
+    if TRACKING_URI:
+        mlflow.set_tracking_uri(TRACKING_URI)
+    
+    model = mlflow.pyfunc.load_model(MODEL_URI)
+    print(f"[INFO] Model loaded successfuly from: {MODEL_URI}")
+    
+    return model
+
+def prediction(data_path: str, model):
     df = pd.read_csv(data_path, header=None)
     df.columns = COLUMNS
 
     result = model.predict(df)
+    print("Prediction Result:", result[0])
     
-    return result[0]
+    return result
 
 if __name__ == "__main__":
-    prediction(TEST_FILE)
+    model = load_model
+    prediction(TEST_FILE, model)
